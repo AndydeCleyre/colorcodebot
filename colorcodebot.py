1bd7f7b7cd7848a43f4afa09e4e6e6938838e89f
Andy Kluger <andykluger@gmail.com>
2018-03-14 01:40:07 -0400

    remove incomplete efforts toward a good inline mode, add personal username contact

--- colorcodebot.py
+++ colorcodebot.py
@@ -1,34 +1,17 @@
 #!/usr/bin/env python3
 import io
-from string import ascii_letters
-# from uuid import uuid4
 
 import yaml
-from plumbum.cmd import highlight  # , wkhtmltopdf
+from plumbum.cmd import highlight
 from telebot import TeleBot
-from telebot.types import (
-    ForceReply,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup
-    # InlineQueryResultDocument
-)
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from vault import TG_API_KEY
 
 
 LANG = {
-    'welcome': (
-        "Hello! Send me a snippet of code as your message text, "
-        "and I'll send you a colorized HTML file. "
-        # "Short snippets can use this bot inline, directly, in any chat "
-        # "(e.g. `@colorcodebot py def hello_world(): print('Hello, World!')` "
-        # "but will send PDFs rather than HTML."
-    ),
-    'query ext': "Fantastic! What type of code is this?",
-    # 'custom ext btn': "Something else",
-    # 'query custom ext': "OK, what's the file extension?",
-    # 'too long for inline': "That's too much for inline usage, let's try one-on-one.",
-    'suggest pm': "Let's chat one-on-one."
+    'welcome': "Hello! Send me a snippet of code as your message text, and I'll send you a colorized HTML file.",
+    'query ext': "Fantastic! What type of code is this?\n\nTo add more types, message @andykluger."
 }
 BOT = TeleBot(TG_API_KEY)
 
@@ -61,38 +44,25 @@ def intake_snippet(message):
         InlineKeyboardButton(
             name, callback_data=ydump({'action': 'set ext', 'ext': ext})
         ) for name, ext in (
-            ('Python', 'py'),
             ('Bash', 'sh'),
+            ('C#', 'cs'),
+            ('C', 'c'),
+            ('CSS', 'css'),
+            ('Go', 'go'),
             ('HTML', 'html'),
-            ('CSS', 'css')
+            ('Java', 'java'),
+            ('JavaScript', 'js'),
+            ('JSON', 'json'),
+            ('Kotlin', 'kt'),
+            ('NGINX', 'nginx'),
+            ('Objective C', 'objc'),
+            ('Python', 'py'),
+            ('Ruby', 'rb'),
+            ('Rust', 'rs'),
+            ('Swift', 'swift')
         )
     ))
-    # kb.add(InlineKeyboardButton(
-    #     LANG['custom ext btn'],
-    #     callback_data=ydump({'action': 'get custom ext'})
-    # ))
     BOT.reply_to(message, LANG['query ext'], reply_markup=kb)
-
-
-# @BOT.callback_query_handler(lambda q: yload(q.data)['action'] == 'get custom ext')
-# def get_custom_filetype(query):
-#     snippet = query.message.reply_to_message
-#     msg = BOT.send_message(
-#         query.message.chat.id,
-#         LANG['query custom ext'],
-#         reply_to_message_id=snippet.message_id,
-#         reply_markup=ForceReply()
-#     )
-#     BOT.register_for_reply(msg, set_snippet_custom_filetype)
-
-
-# def set_snippet_custom_filetype(message):
-#     ext = ''.join(filter(ascii_letters.__contains__, message.text))
-    # data = yload(query.data)
-    # html = mk_html(snippet.text, data['ext'])
-    # with io.StringIO(html) as doc:
-        # doc.name = 'code.html'
-        # BOT.send_document(query.message.chat.id, doc, reply_to_message_id=snippet.message_id)
 
 
 @BOT.callback_query_handler(lambda q: yload(q.data)['action'] == 'set ext')
@@ -105,36 +75,5 @@ def set_snippet_filetype(query):
         BOT.send_document(query.message.chat.id, doc, snippet.message_id)
 
 
-# @BOT.inline_handler(lambda iq: iq.query.split(None, 1)[1:] and len(iq.query) <= 512)
-# def suggest_inline_rendering(inline_query):
-#     BOT.answer_inline_query(
-#         inline_query.id,
-#         [
-#             InlineQueryResultDocument(
-#                 uuid4(),
-#                 'code.pdf',
-#                 ''
-#             )
-#         ],
-#         switch_pm_text=LANG['suggest pm']
-#     )
-    # mk_html()
-    # wkhtmltopdf('-n', '--disable-local-file-access')
-
-
-# @BOT.inline_handler(lambda iq: True)
-# def suggest_private_rendering(inline_query):
-#     BOT.answer_inline_query(inline_query.id, [], switch_pm_text=LANG['suggest pm'])
-    # BOT.answer_inline_query(
-    #     inline_query.id,
-    #     [],
-    #     switch_pm_text=LANG[
-    #         'too long for inline' if len(inline_query.query) > 512 else 'suggest pm'
-    #     ]
-    # )
-
-
 if __name__ == '__main__':
     BOT.polling()
-
-# <filetype> <code to highlight>
