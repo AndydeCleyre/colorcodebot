77d77e96be29b6239d1da6c9d8f99fe022558b40
Andy Kluger <andykluger@gmail.com>
2018-04-24 12:23:47 -0400

    sudo buildah commands (eventually use as user, but until then avoid running whole script as root)

--- mk_image.sh
+++ mk_image.sh
@@ -2,27 +2,26 @@
 appname="colorcodebot"
 version="0.1"
 pyver="3.6"
-ccb=`buildah from alpine:3.7`
+ccb=`sudo buildah from alpine:3.7`
 
-rm -rf app/__pycache__
-rm -rf app/venv
+rm -rf app/__pycache__ app/venv
 
-buildah run $ccb -- adduser -D $appname
-buildah add --chown $appname:$appname $ccb app "/home/$appname/"
+sudo buildah run $ccb -- adduser -D $appname
+sudo buildah add --chown $appname:$appname $ccb app "/home/$appname/"
 
-buildah run $ccb -- apk update
-buildah run $ccb -- apk upgrade
-buildah run $ccb -- apk add fontconfig freetype jpeg python3 s6 zlib
-buildah run $ccb -- apk add freetype-dev gcc jpeg-dev musl-dev python3-dev zlib-dev
+sudo buildah run $ccb -- apk update
+sudo buildah run $ccb -- apk upgrade
+sudo buildah run $ccb -- apk add fontconfig freetype jpeg python3 s6 zlib
+sudo buildah run $ccb -- apk add freetype-dev gcc jpeg-dev musl-dev python3-dev zlib-dev
 
-buildah run $ccb -- pip3 install -U -r /home/$appname/requirements.txt
-buildah run $ccb -- sed -i 's/background-color: #f0f0f0; //g' /usr/lib/python$pyver/site-packages/pygments/formatters/html.py
+sudo buildah run $ccb -- pip3 install -U -r /home/$appname/requirements.txt
+sudo buildah run $ccb -- sed -i 's/background-color: #f0f0f0; //g' /usr/lib/python$pyver/site-packages/pygments/formatters/html.py
 
-buildah run $ccb -- mkdir -p /usr/share/fonts/TTF
-buildah add $ccb /usr/share/fonts/TTF/iosevka-custom-{regular,italic,bold}.ttf /usr/share/fonts/TTF
+sudo buildah run $ccb -- mkdir -p /usr/share/fonts/TTF
+sudo buildah add $ccb /usr/share/fonts/TTF/iosevka-custom-{regular,italic,bold}.ttf /usr/share/fonts/TTF
 
-buildah run $ccb -- apk del freetype-dev gcc jpeg-dev musl-dev python3-dev zlib-dev
-buildah run $ccb -- rm -r /var/cache/apk
+sudo buildah run $ccb -- apk del freetype-dev gcc jpeg-dev musl-dev python3-dev zlib-dev
+sudo buildah run $ccb -- rm -r /var/cache/apk
 
-buildah config --cmd "s6-svscan /home/$appname/svcs" $ccb
-buildah commit --rm $ccb $appname:$version
+sudo buildah config --cmd "s6-svscan /home/$appname/svcs" $ccb
+sudo buildah commit --rm $ccb $appname:$version
