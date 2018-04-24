848c8c9e66ff55cb5bd4fb76b1e08f767e382f2e
Andy Kluger <andykluger@gmail.com>
2018-04-24 14:16:23 -0400

    sudo buildah in the run container script

--- run_container.sh
+++ run_container.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
-ccb=`buildah from colorcodebot:0.1`
+ccb=`sudo buildah from colorcodebot:0.1`
 
 # run, supervised internally (auto-recover), log to stdout:
-buildah run $ccb
+sudo -E buildah run $ccb
 
 # run, supervised internally (auto-recover), log to internal log files:
-# buildah run $ccb -- mv /home/colorcodebot/svcs/colorcodebot/log.down /home/colorcodebot/svcs/colorcodebot/log
-# buildah run $ccb
+# sudo -E buildah run $ccb -- mv /home/colorcodebot/svcs/colorcodebot/log.down /home/colorcodebot/svcs/colorcodebot/log
+# sudo -E buildah run $ccb
 
 # run, unsupervised (no auto-recover), log to stdout:
-# buildah run $ccb -- mv /home/colorcodebot/svcs/colorcodebot/log.down /home/colorcodebot/svcs/colorcodebot/log
-# buildah run $ccb -- /home/colorcodebot/colorcodebot.py
+# sudo -E buildah run $ccb -- mv /home/colorcodebot/svcs/colorcodebot/log.down /home/colorcodebot/svcs/colorcodebot/log
+# sudo -E buildah run $ccb -- /home/colorcodebot/colorcodebot.py
