c81b75888e3103062e2f4c95b6bfd7695057543f
Andy Kluger <andykluger@gmail.com>
2019-03-05 23:53:08 -0500

    remove not-so-useful run-script

--- run_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-ccb=`buildah from colorcodebot:0.2`
-
-# run, supervised internally (auto-recover), log to internal log files:
-buildah run $ccb
-
-# run, unsupervised (no auto-recover), log to stdout:
-# buildah run $ccb -- /home/colorcodebot/colorcodebot.py
