dbf8da691d75fa3abdaf5223f0f2d4aebb0cacb6
Andy Kluger <andykluger@gmail.com>
2021-06-10 14:20:56 -0400

    add help output to mk/reqs.sh

--- mk/reqs.sh
+++ mk/reqs.sh
@@ -2,6 +2,11 @@
 
 root="$(git -C "$(dirname -- "$0")" rev-parse --show-toplevel)"
 
+if [ "$1" ]; then
+  printf '%s\n' 'Upgrade what we can in our *requirements.txt files' 'Args: None' 1>&2
+  exit 1
+fi
+
 for folder in "$root" "$root/app"; do
 
   cd "$folder"
