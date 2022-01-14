b83a66cbce74cb7923d02d8fc7964b4e4719885e
Andy Kluger <AndyKluger@gmail.com>
2022-01-13 23:16:53 -0500

    use a single venv during reqs bump

--- mk/reqs.sh
+++ mk/reqs.sh
@@ -1,23 +1,24 @@
 #!/bin/sh -e
 
-root="$(git -C "$(dirname -- "$0")" rev-parse --show-toplevel)"
-
 if [ "$1" ]; then
   printf '%s\n' 'Upgrade what we can in our *requirements.txt files' 'Args: None' 1>&2
   exit 1
 fi
 
+root="$(git -C "$(dirname -- "$0")" rev-parse --show-toplevel)"
+cd "$root"
+
+if [ ! -d venv ]; then
+  python3 -m venv venv
+fi
+# shellcheck disable=SC1091
+. ./venv/bin/activate
+
+pip install -U pip-tools
+
 for folder in "$root" "$root/app"; do
 
   cd "$folder"
-
-  if [ ! -d venv ]; then
-    python3 -m venv venv
-  fi
-  # shellcheck disable=SC1091
-  . ./venv/bin/activate
-
-  pip install -U pip-tools
 
   for reqsin in *requirements.in; do
     pip-compile -U --no-header --annotation-style line "$reqsin"
