44292c23019bafbfecf2ec6f70fa9c1a634a0d8d
Andy Kluger <AndyKluger@gmail.com>
2022-06-18 03:56:04 -0400

    Add noto color emoji as a final fallback

--- app/colorcodebot.py
+++ app/colorcodebot.py
@@ -131,6 +131,7 @@ def mk_png(code: str, ext: str, theme: str = 'Coldark-Dark', folder=None) -> str
                 'NanumGothicCoding',
                 'Unifont',
                 'Code2000',
+                'Noto Color Emoji'
             ))
         ]
         << code
--- mk/ctnr.sh
+++ mk/ctnr.sh
@@ -40,7 +40,7 @@ today=$(date +%Y.%j)
 tz="America/New_York"
 
 base_img='docker.io/library/archlinux:base'
-pkgs='highlight python silicon sops ttf-joypixels ttf-nerd-fonts-symbols-mono'
+pkgs='highlight noto-fonts-emoji python silicon sops ttf-joypixels ttf-nerd-fonts-symbols-mono'
 aur_pkgs='s6 ttf-code2000 ttf-nanumgothic_coding ttf-unifont'
 build_pkgs='git'
 build_groups='base-devel'
